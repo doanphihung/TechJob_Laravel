721bc64ebe9676ffe8ad28e5b31864390869263c
Dphung <hungq394@gmail.com>
2021-07-29 12:04:51 +0700

    Done API employer register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function employerRegister(Request $request) {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password =  Hash::make($request->password);
+        $user->role = 2;
+        $user->save();
+        if($user) {
+            $company_acronym =   strtoupper(substr(preg_replace('/\s+/', '', $request->name), 0, 3));
+            $company = new Company();
+            $company->acronym = $company_acronym;
+            $company->city_id = $request->city_id;
+            $company->user_id = $user->id;
+            $company->employees = $request->employees;
+            $company->map_link = $request->map_link;
+            $company->save();
+            $company->code = $company_acronym . $company->id . rand(1000, 9999);
+            $company->save();
+            return response()->json(['message' => 'Register successfully!'], 201);
+        }
+        return response()->json(['message' => 'Registration failed!'], 500);
+
+    }
+}
